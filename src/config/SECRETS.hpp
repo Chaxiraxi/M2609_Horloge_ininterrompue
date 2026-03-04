8b7eb5356106d7d695868187fbc2b7454fa41dd7
Chaxiraxi <david.goletta@live.com>
2026-03-04 15:39:52 +0100

    Stop tracking secrets file

--- src/config/SECRETS.hpp
+++
@@ -1,9 +0,0 @@
-/**
- * @file SECRETS.h
- * @brief Configuration file for sensitive data.
- *
- * This file contains sensitive configuration data such as WiFi credentials.
- * It should be excluded from version control if the project is public.
- */
-constexpr char WIFI_SSID[] = "S21 FE de David";
-constexpr char WIFI_PASSWORD[] = "jymk4220";
